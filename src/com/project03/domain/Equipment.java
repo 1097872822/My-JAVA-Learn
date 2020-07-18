eca313769e45fba7c6a1d23995d250ee1855c40a
RRWtest <friend_rrw@163.com>
2020-07-18 21:50:04 +0800

    Revert "Project03"
    This reverts commit 272d4121

--- src/com/project03/domain/Equipment.java
+++
@@ -1,9 +0,0 @@
-package com.project03.domain;
-/**
- *@description:
- *@author: RRW friend_rrw@163.com
- *@create: 2020-07-18 16:59
- */
-public interface Equipment {  //提供接口，让子类实现
-    String getDescription();
-}
