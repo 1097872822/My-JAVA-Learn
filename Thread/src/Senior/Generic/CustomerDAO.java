1d7e820ce6f1492c2da8fcfc613fea31050597a2
RRWtest <friend_rrw@163.com>
2020-07-19 19:53:49 +0800

    Revert "Revert "Revert "Revert "Revert "SeniorForJava"""""
    This reverts commit 5c3c7064

--- Thread/src/Senior/Generic/CustomerDAO.java
+++
@@ -1,9 +0,0 @@
-package Senior.Generic;
-/**
- *@description:
- *@author: RRW friend_rrw@163.com
- *@create: 2020-06-15 19:45
- */
-public class CustomerDAO extends DAO<Customer>{  //操作某个类/表的
-
-}
--- Thread/src/Senior/Generic/SubOrder.java
+++
@@ -1,9 +0,0 @@
-package Senior.Generic;
-/**
- *@description:
- *@author: RRW friend_rrw@163.com
- *@create: 2020-06-15 15:59
- */
-public class SubOrder extends OrderforTEST2<Integer>{
-    //注意这不是泛型类SubOrder
-}
